73e054decc2d2ca6ce0ecfe9c7ee0599e43b22a2
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-05-07 13:09:03 +0530

    second_commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,6 @@
 // Write your code here
+import {Link} from 'react-router-dom'
+
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {IoMdClose} from 'react-icons/io'
 import {AiFillHome} from 'react-icons/ai'
@@ -10,36 +12,71 @@ import 'reactjs-popup/dist/index.css'
 
 import './index.css'
 
+const overlayStyles = {
+  backgroundColor: '#ffffff',
+}
+
 const Header = () => (
   <div className="nav-bar-container">
     <nav className="nav-bar">
-      <img
-        className="app-logo"
-        alt="website logo"
-        src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-      />
+      <Link to="/" className="nav-link">
+        <img
+          className="app-logo"
+          alt="website logo"
+          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+        />
+      </Link>
       <div className="popup-container">
         <Popup
           modal
           trigger={
-            <button className="trigger-button" type="button">
-              <GiHamburgerMenu />
+            <button
+              data-testid="hamburgerIconButton"
+              className="trigger-button"
+              type="button"
+            >
+              <GiHamburgerMenu className="close-icon" />
             </button>
           }
+          overlayStyle={overlayStyles}
+          className="popup-content"
         >
           {close => (
             <div className="close-container">
               <button
+                data-testid="closeButton"
                 className="close-button"
                 type="button"
                 onClick={() => close()}
               >
-                <IoMdClose />
+                <IoMdClose className="close-icon" />
               </button>
-              <div className="home-about-icon-container">
-                <AiFillHome />
-                <BsInfoCircleFill />
-              </div>
+              <ul className="home-about-icon-container">
+                <li className="list-item">
+                  <Link to="/" className="nav-link">
+                    <button
+                      className="home-button"
+                      type="button"
+                      onClick={() => close()}
+                    >
+                      <AiFillHome className="home-icon" />
+                      Home
+                    </button>
+                  </Link>
+                </li>
+                <li className="list-item">
+                  <Link to="/about" className="nav-link">
+                    <button
+                      className="home-button"
+                      type="button"
+                      onClick={() => close()}
+                    >
+                      <BsInfoCircleFill className="home-icon" />
+                      About
+                    </button>
+                  </Link>
+                </li>
+              </ul>
             </div>
           )}
         </Popup>
