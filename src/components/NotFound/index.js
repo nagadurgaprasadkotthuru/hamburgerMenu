37441645ecbdc778653c3c79ea759a2d42753b1f
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-05-07 13:11:16 +0530

    second_commit

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,19 @@
 // Write your code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="home-bg-container">
+    <img
+      className="not-found-image"
+      alt="not found"
+      src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+    />
+    <h1 className="heading">Lost Your Way?</h1>
+    <p className="description">
+      Sorry, we cannot find that page. You will find lots to explore on the home
+      page
+    </p>
+  </div>
+)
+
+export default NotFound
