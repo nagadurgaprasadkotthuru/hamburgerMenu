cd7359f0e83682cf4c1a0c5283c8a0bc391be98e
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-05-07 13:12:54 +0530

    second_commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,19 @@
 // Write your code here
+import './index.css'
+
+const About = () => (
+  <div className="home-bg-container">
+    <img
+      className="home-image-sm"
+      alt="about"
+      src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+    />
+    <img
+      className="home-image-lg"
+      alt="about"
+      src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+    />
+  </div>
+)
+
+export default About
